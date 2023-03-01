e5ce7023734ccfaf95536db084ddd6ad2184593e
liveforone <yc4852@gmail.com>
2023-03-01 15:16:15 +0900

    Add jwt logic.

---
+++ user-service/user-service/src/main/java/intelligent_bank_msa/userservice/jwt/TokenInfo.java
@@ -0,0 +1,15 @@
+package intelligent_bank_msa.userservice.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class TokenInfo {
+
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+}
