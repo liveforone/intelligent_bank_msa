bbbc123f0da324fff1bfbf37c963eea4d6fc6ff2
liveforone <yc4852@gmail.com>
2023-03-01 17:29:51 +0900

    Add hidden method filter.

--- user-service/user-service/src/main/java/intelligent_bank_msa/userservice/UserServiceApplication.java
+++ user-service/user-service/src/main/java/intelligent_bank_msa/userservice/UserServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,4 +16,8 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }
