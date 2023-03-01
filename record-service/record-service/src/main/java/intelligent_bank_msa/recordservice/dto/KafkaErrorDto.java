0c3cab2456614c7a4bfcf2a974136f51218a7bd0
liveforone <yc4852@gmail.com>
2023-03-01 22:49:38 +0900

    Remove kafka error dto.

---
+++ record-service/record-service/src/main/java/intelligent_bank_msa/recordservice/dto/KafkaErrorDto.java
@@ -0,0 +1,15 @@
+package intelligent_bank_msa.recordservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class KafkaErrorDto {
+
+    private String errorMessage;
+}
