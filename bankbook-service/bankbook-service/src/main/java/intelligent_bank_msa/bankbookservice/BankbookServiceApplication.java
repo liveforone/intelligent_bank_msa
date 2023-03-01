4b8b6d61de611f1eea30b06c44769e6de11445bf
liveforone <yc4852@gmail.com>
2023-03-01 17:39:06 +0900

    Add hidden method filter.

--- bankbook-service/bankbook-service/src/main/java/intelligent_bank_msa/bankbookservice/BankbookServiceApplication.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_bank_msa/bankbookservice/BankbookServiceApplication.java
@@ -3,13 +3,28 @@ package intelligent_bank_msa.bankbookservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class BankbookServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankbookServiceApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }
