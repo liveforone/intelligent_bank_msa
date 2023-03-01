d454f2758869ae393e6f3599879e0ee497616541
liveforone <yc4852@gmail.com>
2023-03-01 21:50:31 +0900

    Add record entity logic.

---
+++ record-service/record-service/src/main/java/intelligent_bank_msa/recordservice/model/Record.java
@@ -0,0 +1,56 @@
+package intelligent_bank_msa.recordservice.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Record {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String bankBookNum;
+
+    private long money;
+
+    @Enumerated(value = EnumType.STRING)
+    private RecordState recordState;
+
+    @Column(updatable = false)
+    private int createdYear;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(updatable = false)
+    private Month createdMonth;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @Builder
+    public Record(Long id, String title, String bankBookNum, long money, RecordState recordState, int createdYear, Month createdMonth) {
+        this.id = id;
+        this.title = title;
+        this.bankBookNum = bankBookNum;
+        this.money = money;
+        this.recordState = recordState;
+        this.createdYear = createdYear;
+        this.createdMonth = createdMonth;
+    }
+}
---
+++ record-service/record-service/src/main/java/intelligent_bank_msa/recordservice/model/RecordState.java
@@ -0,0 +1,6 @@
+package intelligent_bank_msa.recordservice.model;
+
+public enum RecordState {
+
+    EXPENSE, INCOME
+}
