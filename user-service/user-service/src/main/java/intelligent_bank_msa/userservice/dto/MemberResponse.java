b629ce98396974ab63ad7b8c3a590b6fe2d3ff01
liveforone <yc4852@gmail.com>
2023-03-01 15:17:16 +0900

    Add request/response dto.

---
+++ user-service/user-service/src/main/java/intelligent_bank_msa/userservice/dto/MemberResponse.java
@@ -0,0 +1,19 @@
+package intelligent_bank_msa.userservice.dto;
+
+import intelligent_bank_msa.userservice.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberResponse {
+
+    private Long id;
+    private String email;
+    private String realName;
+    private Role auth;
+}
