c59d531cb8ad0e44edec8f14c8627368316918cb
liveforone <yc4852@gmail.com>
2023-03-01 20:17:02 +0900

    Add response topic.

--- bankbook-service/bankbook-service/src/main/java/intelligent_bank_msa/bankbookservice/mq/constant/Topic.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_bank_msa/bankbookservice/mq/constant/Topic.java
@@ -4,6 +4,10 @@ public class Topic {
 
     public static final String REQUEST_BANKBOOK_DETAIL = "request-bankbook-detail";
     public static final String RESPONSE_BANKBOOK_DETAIL = "response-bankbook-detail";
-    public static final String INCREASE_BALANCE = "increase-balance";
-    public static final String DECREASE_BALANCE = "decrease-balance";
+
+    public static final String REQUEST_INCREASE_BALANCE = "request-increase-balance";
+    public static final String RESPONSE_INCREASE_BALANCE = "response-increase-balance";
+
+    public static final String REQUEST_DECREASE_BALANCE = "request-decrease-balance";
+    public static final String RESPONSE_DECREASE_BALANCE = "response-decrease-balance";
 }
