c1fe5c66d0adbf76f8fb3f5b4a6d0f57c0eefd03
liveforone <yc4852@gmail.com>
2023-03-01 15:15:57 +0900

    Add config class.

---
+++ user-service/user-service/src/main/java/intelligent_bank_msa/userservice/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package intelligent_bank_msa.userservice.config;
+
+import intelligent_bank_msa.userservice.jwt.JwtAuthenticationFilter;
+import intelligent_bank_msa.userservice.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(
+                                "/",
+                                "/member/signup",
+                                "/member/login"
+                        ).permitAll()
+                        .requestMatchers(
+                                "/admin/**"
+                        ).hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                        .and()
+                        .addFilterBefore(
+                                new JwtAuthenticationFilter(jwtTokenProvider),
+                                UsernamePasswordAuthenticationFilter.class
+                        )
+                )
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                .logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/member/prohibition");
+        return http.build();
+    }
+}
