fd9da8ee6872f5af28502fc2ac99fdf82665d5ea
liveforone <yc4852@gmail.com>
2023-03-01 14:41:27 +0900

    Add fallback controller.

---
+++ gateway-service/gateway-service/src/main/java/intelligent_bank_msa/gatewayservice/controller/FallbackController.java
@@ -0,0 +1,18 @@
+package intelligent_bank_msa.gatewayservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+    @GetMapping("/user")
+    public Mono<String> fallbackUser() {
+        String errorMessage = "유저 서비스의 장애로 접근이 불가능합니다."
+                + "\n이용에 불편을 드려 죄송합니다.";
+        return Mono.just(errorMessage);
+    }
+}
