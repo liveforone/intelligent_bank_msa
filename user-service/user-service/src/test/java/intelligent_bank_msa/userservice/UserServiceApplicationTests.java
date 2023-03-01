c825a148421ae8a2103faeb03f7a44db0ea578a6
liveforone <yc4852@gmail.com>
2023-03-01 14:53:48 +0900

    Add user-service.

---
+++ user-service/user-service/src/test/java/intelligent_bank_msa/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package intelligent_bank_msa.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
