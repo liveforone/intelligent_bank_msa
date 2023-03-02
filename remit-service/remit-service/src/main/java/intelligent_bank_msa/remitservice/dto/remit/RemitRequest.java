c6f4713970355663c15eb3b5c56f6f24f158213d
liveforone <yc4852@gmail.com>
2023-03-02 14:16:27 +0900

    Add remit dto.

---
+++ remit-service/remit-service/src/main/java/intelligent_bank_msa/remitservice/dto/remit/RemitRequest.java
@@ -0,0 +1,23 @@
+package intelligent_bank_msa.remitservice.dto.remit;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class RemitRequest {
+
+    @NotNull(message = "금액을 입력하세요.")
+    private long inputMoney;
+
+    @Size(min = 13, max = 13, message = "계좌번호는 13자리입니다. 정확히 입력해주세요.")
+    private String bankBookNum;
+
+    @NotBlank(message = "비밀번호를 정확히 입력해주세요.")
+    private String password;
+
+    private String myBankBookNum;
+}
