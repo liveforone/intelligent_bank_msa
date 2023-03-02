e3d30bf2525b38af8acf6c77e1e1f1d650d25e61
liveforone <yc4852@gmail.com>
2023-03-02 15:40:47 +0900

    Add copy entity and calculate mapper.

---
+++ calculate-service/calculate-service/src/main/java/intelligent_bank_msa/calculateservice/utility/CalculateMapper.java
@@ -0,0 +1,14 @@
+package intelligent_bank_msa.calculateservice.utility;
+
+
+import intelligent_bank_msa.calculateservice.dto.calculate.CalculateResponse;
+
+public class CalculateMapper {
+
+    public static CalculateResponse dtoBuilder(Long sumExpense, Long sumIncome) {
+        return CalculateResponse.builder()
+                .expense((sumExpense == null) ? 0 : sumExpense)
+                .income((sumIncome == null) ? 0 : sumIncome)
+                .build();
+    }
+}
---
+++ calculate-service/calculate-service/src/main/java/intelligent_bank_msa/calculateservice/utility/RecordCopyMapper.java
@@ -0,0 +1,19 @@
+package intelligent_bank_msa.calculateservice.utility;
+
+import intelligent_bank_msa.calculateservice.dto.record.RecordRequest;
+import intelligent_bank_msa.calculateservice.model.RecordCopy;
+
+public class RecordCopyMapper {
+
+    public static RecordCopy dtoToEntity(RecordRequest recordRequest) {
+        return RecordCopy.builder()
+                .id(recordRequest.getId())
+                .title(recordRequest.getTitle())
+                .bankBookNum(recordRequest.getBankBookNum())
+                .money(recordRequest.getMoney())
+                .recordState(recordRequest.getRecordState())
+                .createdYear(recordRequest.getCreatedYear())
+                .createdMonth(recordRequest.getCreatedMonth())
+                .build();
+    }
+}
