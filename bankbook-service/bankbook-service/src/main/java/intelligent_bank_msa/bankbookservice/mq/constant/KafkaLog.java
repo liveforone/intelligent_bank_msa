aa7f4ff412b5ca11fd1a5c7ee04bb7c8c427728d
liveforone <yc4852@gmail.com>
2023-03-02 10:13:27 +0900

    Remove bankbook detail logic.

--- bankbook-service/bankbook-service/src/main/java/intelligent_bank_msa/bankbookservice/mq/constant/KafkaLog.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_bank_msa/bankbookservice/mq/constant/KafkaLog.java
@@ -4,5 +4,4 @@ public class KafkaLog {
 
     public static final String SEND_ERROR_MESSAGE_LOG = "BankBook Producer send Error Message";
     public static final String KAFKA_RECEIVE_LOG = "Consumer receive Kafka Message -> ";
-    public static final String SEND_BANKBOOK_DETAIL_LOG = "BankBook Producer send BankBook Detail data";
 }
