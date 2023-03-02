00ed2756b8732267111866fe8a40b1c7395b83db
liveforone <yc4852@gmail.com>
2023-03-02 14:17:02 +0900

    Add password check dto

---
+++ remit-service/remit-service/src/main/java/intelligent_bank_msa/remitservice/dto/bankbook/PasswordCheckResponse.java
@@ -0,0 +1,11 @@
+package intelligent_bank_msa.remitservice.dto.bankbook;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PasswordCheckResponse {
+
+    private String status;
+}
---
+++ remit-service/remit-service/src/main/java/intelligent_bank_msa/remitservice/dto/bankbook/PasswordStatus.java
@@ -0,0 +1,6 @@
+package intelligent_bank_msa.remitservice.dto.bankbook;
+
+public enum PasswordStatus {
+
+    TRUE, FALSE
+}
