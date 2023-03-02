9ab82dffd7ceec9cbabef14b315c96182b3887dc
liveforone <yc4852@gmail.com>
2023-03-02 14:17:41 +0900

    Add kafka producer

---
+++ remit-service/remit-service/src/main/java/intelligent_bank_msa/remitservice/mq/RemitProducer.java
@@ -0,0 +1,46 @@
+package intelligent_bank_msa.remitservice.mq;
+
+import com.google.gson.Gson;
+import intelligent_bank_msa.remitservice.dto.bankbook.BalanceRequest;
+import intelligent_bank_msa.remitservice.dto.record.RecordRequest;
+import intelligent_bank_msa.remitservice.dto.remit.RemitRequest;
+import intelligent_bank_msa.remitservice.mq.constant.Topic;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RemitProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    Gson gson = new Gson();
+
+    public void requestIncreaseBalance(RemitRequest remitRequest) {
+        BalanceRequest balanceRequest = BalanceRequest.makeBalanceRequest(
+                remitRequest.getInputMoney(),
+                remitRequest.getBankBookNum()
+        );
+        String jsonOrder = gson.toJson(balanceRequest);
+
+        kafkaTemplate.send(Topic.REQUEST_INCREASE_BALANCE, jsonOrder);
+    }
+
+    public void requestDecreaseBalance(RemitRequest remitRequest) {
+        BalanceRequest balanceRequest = BalanceRequest.makeBalanceRequest(
+                remitRequest.getInputMoney(),
+                remitRequest.getMyBankBookNum()
+        );
+        String jsonOrder = gson.toJson(balanceRequest);
+
+        kafkaTemplate.send(Topic.REQUEST_DECREASE_BALANCE, jsonOrder);
+    }
+
+    public void requestSaveRecord(RecordRequest request) {
+        String jsonOrder = gson.toJson(request);
+
+        kafkaTemplate.send(Topic.REQUEST_SAVE_RECORD, jsonOrder);
+    }
+}
