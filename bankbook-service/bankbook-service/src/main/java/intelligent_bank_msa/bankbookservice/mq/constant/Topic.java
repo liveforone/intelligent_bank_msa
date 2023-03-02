6c87020e08513f32d22027c0e3e965d72babb9c9
liveforone <yc4852@gmail.com>
2023-03-02 10:13:32 +0900

    Remove bankbook detail logic.

--- bankbook-service/bankbook-service/src/main/java/intelligent_bank_msa/bankbookservice/mq/constant/Topic.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_bank_msa/bankbookservice/mq/constant/Topic.java
@@ -2,9 +2,6 @@ package intelligent_bank_msa.bankbookservice.mq.constant;
 
 public class Topic {
 
-    public static final String REQUEST_BANKBOOK_DETAIL = "request-bankbook-detail";
-    public static final String RESPONSE_BANKBOOK_DETAIL = "response-bankbook-detail";
-
     public static final String REQUEST_INCREASE_BALANCE = "request-increase-balance";
     public static final String RESPONSE_INCREASE_BALANCE = "response-increase-balance";
 
