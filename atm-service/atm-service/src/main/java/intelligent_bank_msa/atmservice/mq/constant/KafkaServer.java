f500b5c1af42d70eace99357af18079ebe7371ce
liveforone <yc4852@gmail.com>
2023-03-07 20:44:42 +0900

    Refactor kafka server port num.

--- atm-service/atm-service/src/main/java/intelligent_bank_msa/atmservice/mq/constant/KafkaServer.java
+++ atm-service/atm-service/src/main/java/intelligent_bank_msa/atmservice/mq/constant/KafkaServer.java
@@ -2,5 +2,5 @@ package intelligent_bank_msa.atmservice.mq.constant;
 
 public class KafkaServer {
 
-    public static final String KAFKA_SERVER_POST = "localhost:9092";
+    public static final String KAFKA_SERVER_POST = "172.18.0.101:9092";
 }
