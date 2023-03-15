64c8334aec06736f4e428de5aba51d10f9c03875
liveforone <yc4852@gmail.com>
2023-03-15 21:13:23 +0900

    Add list mapper.

--- user-service/user-service/src/main/java/intelligent_bank_msa/userservice/service/util/MemberMapper.java
+++ user-service/user-service/src/main/java/intelligent_bank_msa/userservice/service/util/MemberMapper.java
@@ -1,9 +1,12 @@
-package intelligent_bank_msa.userservice.utility;
+package intelligent_bank_msa.userservice.service.util;
 
 
 import intelligent_bank_msa.userservice.dto.MemberResponse;
 import intelligent_bank_msa.userservice.dto.MemberSignupRequest;
 import intelligent_bank_msa.userservice.domain.Member;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MemberMapper {
 
@@ -17,12 +20,19 @@ public class MemberMapper {
                 .build();
     }
 
-    public static MemberResponse dtoBuilder(Member member) {
+    public static MemberResponse entityToDto(Member member) {
         return MemberResponse.builder()
                 .id(member.getId())
                 .email(member.getEmail())
                 .realName(member.getRealName())
                 .auth(member.getAuth())
                 .build();
+    }
+
+    public static List<MemberResponse> entityToDtoList(List<Member> members) {
+        return members
+                .stream()
+                .map(MemberMapper::entityToDto)
+                .collect(Collectors.toList());
     }
 }
