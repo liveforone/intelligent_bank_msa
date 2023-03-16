7bfd5fdfca3f5c6c1f5a1f4a7e1cb483a34b5b28
liveforone <yc4852@gmail.com>
2023-03-16 21:06:23 +0900

    Add bankbook url constants.

---
+++ remit-service/remit-service/src/main/java/intelligent_bank_msa/remitservice/feign/BankBookUrl.java
@@ -0,0 +1,7 @@
+package intelligent_bank_msa.remitservice.feign;
+
+public class BankBookUrl {
+
+    public static final String BASE = "bankbook-service";
+    public static final String BANK_INFO_REMIT = "/bank-info/remit";
+}
