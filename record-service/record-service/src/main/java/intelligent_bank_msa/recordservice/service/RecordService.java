6c3c35e9449dcfcff78bbcf1b7d2714dfb6e8de9
liveforone <yc4852@gmail.com>
2023-03-18 10:50:36 +0900

    Remove un use dependency.

--- record-service/record-service/src/main/java/intelligent_bank_msa/recordservice/service/RecordService.java
+++ record-service/record-service/src/main/java/intelligent_bank_msa/recordservice/service/RecordService.java
@@ -4,8 +4,6 @@ import intelligent_bank_msa.recordservice.dto.RecordResponse;
 import intelligent_bank_msa.recordservice.repository.RecordRepository;
 import intelligent_bank_msa.recordservice.utility.RecordMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
